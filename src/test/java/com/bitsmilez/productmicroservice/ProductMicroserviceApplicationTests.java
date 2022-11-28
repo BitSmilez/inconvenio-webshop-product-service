a5e1fa53d113f54ffc4bcd91477d8709c19af1d0
jutnh <jutnhbr@gmail.com>
2022-11-28 16:51:48 +0100

    [/] Initialized Spring Boot + Maven Project

---
+++ src/test/java/com/bitsmilez/productmicroservice/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsmilez.productmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
