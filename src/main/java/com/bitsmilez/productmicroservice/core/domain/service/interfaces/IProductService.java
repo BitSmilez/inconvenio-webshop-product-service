48727214f6cc54306b0a18ffad531f8dcee2ca7f
Biri <test@email>
2022-12-10 18:01:02 +0100

    INCONV-61 [/] removed annotation

--- src/main/java/com/bitsmilez/productmicroservice/core/domain/service/interfaces/IProductService.java
+++ src/main/java/com/bitsmilez/productmicroservice/core/domain/service/interfaces/IProductService.java
@@ -1,12 +1,10 @@
 package com.bitsmilez.productmicroservice.core.domain.service.interfaces;
 
 import com.bitsmilez.productmicroservice.core.domain.service.imp.ProductDto;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
 public interface IProductService {
     
     List<ProductDto> getAllProducts();
