5e6a76f9b3b67b1a1f13fe7bd222693cc316e95b
Biri <test@email>
2022-12-10 11:03:11 +0100

    INCONV-61 [+] added ModelWrapper for DTO

--- src/main/java/com/bitsmilez/productmicroservice/ProductMicroserviceApplication.java
+++ src/main/java/com/bitsmilez/productmicroservice/ProductMicroserviceApplication.java
@@ -1,14 +1,32 @@
 package com.bitsmilez.productmicroservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class ProductMicroserviceApplication {
+public class ProductMicroserviceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductMicroserviceApplication.class, args);
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
+
+	public static void main(String[] args) {
+		//Step1
+		SpringApplication.run(ProductMicroserviceApplication.class, args);
+		//Step3
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//step2
+
+
+	}
 }
