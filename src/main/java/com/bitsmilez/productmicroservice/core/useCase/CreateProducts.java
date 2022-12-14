4552491544035eea465619b97604dec7ad781173
jutnh <jutnhbr@gmail.com>
2022-12-15 00:00:14 +0100

    [+] Changed to BigDecimal [/] Some salesPrice variety

--- src/main/java/com/bitsmilez/productmicroservice/core/useCase/CreateProducts.java
+++ src/main/java/com/bitsmilez/productmicroservice/core/useCase/CreateProducts.java
@@ -4,6 +4,7 @@ import com.bitsmilez.productmicroservice.core.domain.model.Categories;
 import com.bitsmilez.productmicroservice.core.domain.service.imp.ProductDto;
 import com.bitsmilez.productmicroservice.core.domain.service.interfaces.IProductService;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 
 
@@ -11,7 +12,8 @@ public class CreateProducts {
 
 
     IProductService productService;
-    
+
+
 
     public CreateProducts(IProductService productService) {
         this.productService = productService;
@@ -20,10 +22,11 @@ public class CreateProducts {
     public  void createProducts(){
         LinkedList<ProductDto> products = new LinkedList<>();
 
+
         ProductDto boots = new ProductDto();
         boots.setName("The best boots you ever saw");
-        boots.setPrice(420.);
-        boots.setSalesPrice(69.69);
+        boots.setPrice(new BigDecimal("420.00"));
+        boots.setSalesPrice(null);
         boots.setImg("https://ucarecdn.com/81152736-d006-4fbc-8582-a60f0f8d0683/");
         boots.setDescription("I sure hope it wont rain when you wear these awesome boots!");
         boots.setStock(5);
@@ -32,8 +35,8 @@ public class CreateProducts {
 
         ProductDto mug = new ProductDto();
         mug.setName("THE mug");
-        mug.setPrice(420.);
-        mug.setSalesPrice(69.69);
+        mug.setPrice(new BigDecimal("420.00"));
+        mug.setSalesPrice(new BigDecimal("69.69"));
         mug.setImg("https://ucarecdn.com/8f73e3b8-bc9c-46d1-a0f7-1bad02629692/");
         mug.setDescription("I dont think its a good idea to wear a white shirt with that one buddy.");
         mug.setStock(5);
@@ -44,8 +47,8 @@ public class CreateProducts {
 
         ProductDto wineGlass = new ProductDto();
         wineGlass.setName("Best wine glass ever");
-        wineGlass.setPrice(420.);
-        wineGlass.setSalesPrice(69.69);
+        wineGlass.setPrice(new BigDecimal("420.00"));
+        wineGlass.setSalesPrice(new BigDecimal("69.69"));
         wineGlass.setImg("https://ucarecdn.com/ba2add07-bb0e-4e23-8081-cd90b936aa95/");
         wineGlass.setDescription("Brother if you buy that, you got some problems.");
         wineGlass.setStock(5);
@@ -55,8 +58,8 @@ public class CreateProducts {
 
         ProductDto champagneGlass = new ProductDto();
         champagneGlass.setName("Lets have champagne glass");
-        champagneGlass.setPrice(420.);
-        champagneGlass.setSalesPrice(69.69);
+        champagneGlass.setPrice(new BigDecimal("420.00"));
+        boots.setSalesPrice(null);
         champagneGlass.setImg("https://ucarecdn.com/9f95c6c4-ace3-4f59-a0da-e0c15591e002/");
         champagneGlass.setDescription("For our fellow buddy with two mouths (or dumbasses)");
         champagneGlass.setStock(5);
@@ -66,8 +69,8 @@ public class CreateProducts {
 
         ProductDto fork = new ProductDto();
         fork.setName("I never wanna use a diffrent fork FORK!");
-        fork.setPrice(420.);
-        fork.setSalesPrice(69.69);
+        fork.setPrice(new BigDecimal("420.00"));
+        fork.setSalesPrice(new BigDecimal("69.69"));
         fork.setImg("https://ucarecdn.com/ac45e974-a77e-4f1c-86d1-f189204d18df/");
         fork.setDescription("I sure hope it wont rain when you wear these awesome spoon!");
         fork.setStock(5);
