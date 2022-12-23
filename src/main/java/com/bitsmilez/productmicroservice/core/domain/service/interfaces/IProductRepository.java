8bf4d4220126d299ce584fe37ace093ae33afe2c
Biri <test@email>
2022-12-23 18:48:15 +0100

    INCONV-5 [/] refactor input type

--- src/main/java/com/bitsmilez/productmicroservice/core/domain/service/interfaces/IProductRepository.java
+++ src/main/java/com/bitsmilez/productmicroservice/core/domain/service/interfaces/IProductRepository.java
@@ -1,6 +1,7 @@
 package com.bitsmilez.productmicroservice.core.domain.service.interfaces;
 
 
+import com.bitsmilez.productmicroservice.core.domain.model.Categories;
 import com.bitsmilez.productmicroservice.core.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +13,6 @@ import java.util.UUID;
 
 @Repository
 public interface IProductRepository extends JpaRepository<Product, UUID> {
-    @Query(value= "SELECT p FROM Product p WHERE LOWER(p.category) LIKE %?1%")
-    List<Product> findAllByCategory(String keyword);
+    @Query(value= "SELECT p FROM Product p WHERE p.category = ?1")
+    List<Product> findAllByCategory(Categories Category);
 }
