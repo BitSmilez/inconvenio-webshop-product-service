2e3297c8f9987838000f665ba94c63ac98bb952e
jutnh <jutnhbr@gmail.com>
2023-01-13 22:23:33 +0100

    INCONV-20 [+] Added new MQConfig for 2 queues and routing keys to send messages over the same exchange

--- src/main/java/com/bitsmilez/productmicroservice/config/MQConfig.java
+++ src/main/java/com/bitsmilez/productmicroservice/config/MQConfig.java
@@ -11,24 +11,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MQConfig {
 
-    public static final String PRODUCT_QUEUE = "product-cart-queue";
-    public static final String PRODUCT_EXCHANGE_TOPIC = "add.to.cart";
-    public static final String PRODUCT_ROUTING_KEY = "productservice.addtocart";
+    public static final String ADD_PRODUCT_QUEUE = "add-product-cart-queue";
+    public static final String REMOVE_PRODUCT_QUEUE = "remove-product-cart-queue";
 
-    @Bean
-    public Queue productQueue() {
-        return new Queue(PRODUCT_QUEUE);
-    }
+    public static final String PRODUCT_EXCHANGE = "product_exchange";
 
-    @Bean
-    public TopicExchange productExchange() {
-        return new TopicExchange(PRODUCT_EXCHANGE_TOPIC);
-    }
+    public static final String PRODUCT_TOPIC_ADD_TO_CART = "productservice.add";
+    public static final String PRODUCT_TOPIC_REMOVE_FROM_CART = "productservice.remove";
 
-    @Bean
-    public Binding productBinding(Queue productQueue, TopicExchange productExchange) {
-        return BindingBuilder.bind(productQueue).to(productExchange).with(PRODUCT_ROUTING_KEY);
-    }
+
+
+        @Bean
+        Queue addProductQueue() {
+            return new Queue(ADD_PRODUCT_QUEUE, false);
+        }
+
+        @Bean
+        Queue removeProductQueue() {
+            return new Queue(REMOVE_PRODUCT_QUEUE, false);
+        }
+
+        @Bean
+        TopicExchange productExchange() {
+            return new TopicExchange(PRODUCT_EXCHANGE);
+        }
+
+        @Bean
+        Binding addItemBinding(Queue addProductQueue, TopicExchange productExchange) {
+            return BindingBuilder.bind(addProductQueue).to(productExchange).with(PRODUCT_TOPIC_ADD_TO_CART);
+        }
+
+        @Bean
+        Binding removeItemBinding(Queue removeProductQueue, TopicExchange productExchange) {
+            return BindingBuilder.bind(removeProductQueue).to(productExchange).with(PRODUCT_TOPIC_REMOVE_FROM_CART);
+        }
+
 
     @Bean
     public MessageConverter productMessageJsonConverter() {
