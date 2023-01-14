b4b1217e59a89540136688a4974b3ab16ce6877b
jutnh <jutnhbr@gmail.com>
2023-01-14 20:43:08 +0100

    INCONV-19 [+] Added Update Queue and Topic / Routing Key + Binding

--- src/main/java/com/bitsmilez/productmicroservice/config/MQConfig.java
+++ src/main/java/com/bitsmilez/productmicroservice/config/MQConfig.java
@@ -13,11 +13,13 @@ public class MQConfig {
 
     public static final String ADD_PRODUCT_QUEUE = "add-product-cart-queue";
     public static final String REMOVE_PRODUCT_QUEUE = "remove-product-cart-queue";
+    public static final String UPDATE_PRODUCT_QUEUE = "update-product-cart-queue";
 
     public static final String PRODUCT_EXCHANGE = "product_exchange";
 
     public static final String PRODUCT_TOPIC_ADD_TO_CART = "productservice.add";
     public static final String PRODUCT_TOPIC_REMOVE_FROM_CART = "productservice.remove";
+    public static final String PRODUCT_TOPIC_UPDATE_CART = "productservice.update";
 
 
 
@@ -29,6 +31,11 @@ public class MQConfig {
         @Bean
         Queue removeProductQueue() {
             return new Queue(REMOVE_PRODUCT_QUEUE, false);
+        }
+
+        @Bean
+        Queue updateProductQueue() {
+            return new Queue(UPDATE_PRODUCT_QUEUE, false);
         }
 
         @Bean
@@ -44,6 +51,11 @@ public class MQConfig {
         @Bean
         Binding removeItemBinding(Queue removeProductQueue, TopicExchange productExchange) {
             return BindingBuilder.bind(removeProductQueue).to(productExchange).with(PRODUCT_TOPIC_REMOVE_FROM_CART);
+        }
+
+        @Bean
+        Binding updateItemBinding(Queue updateProductQueue, TopicExchange productExchange) {
+            return BindingBuilder.bind(updateProductQueue).to(productExchange).with(PRODUCT_TOPIC_UPDATE_CART);
         }
 
 
